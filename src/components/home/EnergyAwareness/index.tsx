ed12f1b2bd102d65f4ccbd60742b77ce369191b9
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-04-28 03:19:39 -0700

    src/components/home/EnergyAwareness/index.tsx

--- src/components/home/EnergyAwareness/index.tsx
+++ src/components/home/EnergyAwareness/index.tsx
@@ -2,32 +2,29 @@ import { motion } from "framer-motion";
 
 const EnergyAwareness = () => {
   const motionProps = {
-    initial: { x: -200 }, // Content starts from left
-    animate: { x: 0 }, // Moves to its original position
-    transition: { duration: 0.6 }, // Same duration for both
+    initial: { x: -200 },
+    animate: { x: 0 },
+    transition: { duration: 0.6 },
   };
 
   const motionImageProps = {
-    initial: { x: 200 }, // Image starts from right
-    animate: { x: 0 }, // Image moves to its original position
-    transition: { duration: 0.6 }, // Same duration for both
+    initial: { x: 200 },
+    animate: { x: 0 },
+    transition: { duration: 0.6 },
   };
 
   return (
     <>
       <section className="flex flex-col md:flex-row items-start justify-between py-12 px-6 bg-[#FAF5EF]">
         {/* Content section */}
-        <motion.div
-          className="md:w-1/2 text-left"
-          {...motionProps} // Apply motion props to content section
-        >
+        <motion.div className="md:w-1/2 w-full text-left" {...motionProps}>
           <div className="w-9 border-t-2 border-[#F7A235] mb-4"></div>
 
           <motion.h2
             className="text-xl font-normal text-[#333] mb-4"
             initial={{ x: -200 }}
             animate={{ x: 0 }}
-            transition={{ duration: 0.6, delay: 0.2 }} // Faster animation for heading
+            transition={{ duration: 0.6, delay: 0.2 }}
           >
             Ecological Way
           </motion.h2>
@@ -36,7 +33,7 @@ const EnergyAwareness = () => {
             className="text-4xl font-bold text-[#333] mb-6"
             initial={{ x: -200 }}
             animate={{ x: 0 }}
-            transition={{ duration: 0.6, delay: 0.4 }} // Faster animation for paragraph
+            transition={{ duration: 0.6, delay: 0.4 }}
           >
             Reshaping Energy for <br /> the Future
           </motion.p>
@@ -45,7 +42,7 @@ const EnergyAwareness = () => {
             className="text-xl font-normal text-gray-600 mb-8"
             initial={{ x: -200 }}
             animate={{ x: 0 }}
-            transition={{ duration: 0.6, delay: 0.6 }} // Faster animation for paragraph
+            transition={{ duration: 0.6, delay: 0.6 }}
           >
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
             tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
@@ -56,19 +53,20 @@ const EnergyAwareness = () => {
               className="md:w-1/2 w-full"
               initial={{ x: -200 }}
               animate={{ x: 0 }}
-              transition={{ duration: 0.6, delay: 0.8 }} // Faster animation for content block
+              transition={{ duration: 0.6, delay: 0.8 }}
             >
-              <span className="flex items-center">
+              <span className="flex flex-col items-center">
                 <img
                   src="images/ReducedailyEnergy.png"
                   alt="Energy Icon"
-                  className="w-8 h-8 mr-2"
+                  className="w-8 h-8 mb-2"
                 />
-                <h2 className="text-lg font-semibold">
-                  Reduce Your Daily Energy Use
+                <h2 className="text-lg font-semibold text-center">
+                  Reduce Your Daily <br />
+                  Energy Use
                 </h2>
               </span>
-              <p className="text-sm">
+              <p className="text-normal text-1xl text-center">
                 Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit.
               </p>
             </motion.div>
@@ -77,30 +75,28 @@ const EnergyAwareness = () => {
               className="md:w-1/2 w-full"
               initial={{ x: -200 }}
               animate={{ x: 0 }}
-              transition={{ duration: 0.6, delay: 1 }} // Faster animation for content block
+              transition={{ duration: 0.6, delay: 1 }}
             >
-              <span className="flex items-center">
+              <span className="flex flex-col items-center">
                 <img
-                  src="images/ReducedailyEnergy.png"
+                  src="images/TheMostusedEnergysystem.png"
                   alt="Energy Icon"
-                  className="w-8 h-8 mr-2"
+                  className="w-8 h-8 mb-2"
                 />
-                <h2 className="text-lg font-semibold">
-                  Reduce Your Daily Energy Use
+                <h2 className="text-lg font-semibold text-center">
+                  The Most Used <br />
+                  Energy Systems
                 </h2>
               </span>
-              <p className="text-sm">
-                Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit.
+              <p className="text-sm text-center">
+                Lorem ipsum dolor sit amet,
+                <br /> consectetur adipiscing elit.
               </p>
             </motion.div>
           </div>
         </motion.div>
 
-        {/* Image section */}
-        <motion.div
-          className="md:w-1/2 w-full"
-          {...motionImageProps} // Apply motion props to image section (move right to left)
-        >
+        <motion.div className="md:w-1/2 w-full" {...motionImageProps}>
           <img
             src="images/EnergyReshapping.jpg"
             alt="Energy Systems"
