5776e4879f55eb8387dca021b068c7ef930e2242
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-04-29 12:46:12 -0700

    footer add in the last

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -1,11 +1,12 @@
 import EnergyProjects from "../../components/projects";
 import ServiceTabs from "../../components/projects/SolarServiceTabs";
-
+import Footer from "../../components/home/footer";
 const Project = () => {
   return (
     <>
       <EnergyProjects />
       <ServiceTabs />
+      <Footer />
     </>
   );
 };
