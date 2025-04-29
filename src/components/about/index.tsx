37f2f597869a8705ba66abccfbc2142840e9c7b4
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-04-29 08:27:23 -0700

    removed extra comments

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -1,14 +1,12 @@
 const GreenTech = () => {
   return (
     <div className="relative h-96 w-full">
-      {/* Image with more darkness */}
       <img
         src="/images/solarinstallation.jpg"
         alt="Solar Installation"
         className="w-full h-full object-cover object-top brightness-30"
       />
 
-      {/* Overlay content */}
       <div className="absolute inset-0 z-50 flex flex-col items-center justify-center text-center px-4">
         <h1 className="text-white text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold">
           About
