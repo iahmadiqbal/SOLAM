5a9962c8435fa1f9e2e41bd692fd90983ac018dd
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-04-29 02:44:07 -0700

    about  componennt added

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,11 +1,15 @@
 import GreenTech from "../../components/about";
 import SolarEssentials from "../../components/about/SolarEssentials";
+import EnergyAwareness from "../../components/home/EnergyAwareness";
+import KeyMetrics from "../../components/home/KeyMetrics";
 
 const About = () => {
   return (
     <>
       <GreenTech />
       <SolarEssentials />
+      <EnergyAwareness />
+      <KeyMetrics />
     </>
   );
 };
