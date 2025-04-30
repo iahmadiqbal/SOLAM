b73f7bec58793755cd656532af4317b0cd292b8e
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-04-30 01:20:12 -0700

    footer section added in the last

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,6 +1,7 @@
 import ConnectWithUs from "../../components/contact/ConnectWithUs";
 import ContactMap from "../../components/contact/ContactMap";
 import NeedAssistance from "../../components/contact/NeedAssistance";
+import Footer from "../../components/home/footer";
 
 const Contact = () => {
   return (
@@ -8,6 +9,7 @@ const Contact = () => {
       <NeedAssistance />
       <ContactMap />
       <ConnectWithUs />
+      <Footer />
     </>
   );
 };
